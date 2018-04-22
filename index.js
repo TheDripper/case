6327dbda4155e4451e37f8f8d4502ff9842cb1f5
TheDripper <tylerehill@gmail.com>
2018-04-21 19:46:57 -0700

    sqlluv

--- index.js
+++ index.js
@@ -1,55 +1,19 @@
-let Imap = require('imap');
-let inspect = require('util').inspect;
-
-var imap = new Imap({
-	user: "drownlux@gmail.com",
-	password: "1BelVita!",
-	host: "imap.gmail.com",
-	port: 993,
-	tls: true
+const express = require('express');
+const app = express();
+const axios = require('axios');
+const knex = require('knex')({
+	client: 'mysql',
+	connection: {
+		host: '127.0.0.1',
+		user: 'root',
+		password: 'root',
+		database: 'front'
+	}
 });
-
-function openInbox(cb) {
-	imap.openBox('INBOX', true, cb);
-}
-
-imap.once('ready',function(){
-	openInbox((err,box)=>{
-		let f = imap.seq.fetch('1:2', {
-			bodies: ''
-		});
-		f.on('message',(msg,seqno)=>{
-			var prefix = '(#' + seqno + ') ';
-			msg.on('body',(str,fo)=>{
-				let buffer = '';
-				str.on('data',chunk=>{
-					buffer += chunk.toString('utf8');
-				});
-				str.once('end',()=>{
-					console.log(prefix + 'Parsed header: %s', inspect(Imap.parseHeader(buffer)));
-				});
-			});
-      			msg.once('attributes', function(attrs) {
-      			  console.log(prefix + 'Attributes: %s', inspect(attrs, false, 8));
-      			});
-      			msg.once('end', function() {
-      			  console.log(prefix + 'Finished');
-      			});
-		});
-    		f.once('error', function(err) {
-    		  console.log('Fetch error: ' + err);
-    		});
-    		f.once('end', function() {
-    		  console.log('Done fetching all messages!');
-    		  imap.end();
-    		});
+app.get('/',(req,res)=>{
+	knex('wp_posts').select('post_title').then(posts=>{
+		console.log(posts);
+		res.send(posts);
 	});
 });
-imap.once('error', function(err) {
-  console.log(err);
-});
-imap.once('end',()=>{
-	console.log('ned');
-});
-
-imap.connect();
+app.listen(3000,()=>console.log('brick'));
